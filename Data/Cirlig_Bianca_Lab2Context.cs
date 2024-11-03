a766ac00f7d3ba2d88e5ce2a29b1dd328117599c
Bubblebby <geanina.cirlig@stud.ubbcluj.ro>
2024-11-03 18:33:19 +0200

    Add project files.

---
+++ Data/Cirlig_Bianca_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cirlig_Bianca_Lab2.Models;
+
+namespace Cirlig_Bianca_Lab2.Data
+{
+    public class Cirlig_Bianca_Lab2Context : DbContext
+    {
+        public Cirlig_Bianca_Lab2Context (DbContextOptions<Cirlig_Bianca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cirlig_Bianca_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Cirlig_Bianca_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Cirlig_Bianca_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Cirlig_Bianca_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
